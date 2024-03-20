5b909d821d9d07caf34539ba9ced45dcf0059496
susantimilsinaVaricon <132641485+susantimilsinaVaricon@users.noreply.github.com>
2024-03-20 11:55:50 +0545

    small fixes

--- lib/src/form_builder/varicon_form_builder.dart
+++ lib/src/form_builder/varicon_form_builder.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:convert';
 import 'dart:developer';
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
