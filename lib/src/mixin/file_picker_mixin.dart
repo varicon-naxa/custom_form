35e3898da62254e74699ca1019006df8805a6350
susantimilsinaVaricon <132641485+susantimilsinaVaricon@users.noreply.github.com>
2024-07-16 17:53:00 +0545

    downgrading html enhanced to 2.5.1

--- lib/src/mixin/file_picker_mixin.dart
+++ lib/src/mixin/file_picker_mixin.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:fluttertoast/fluttertoast.dart';
