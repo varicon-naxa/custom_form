4367f573db5649ee23e1b82d901853786bebcd58
susantimilsinaVaricon <132641485+susantimilsinaVaricon@users.noreply.github.com>
2025-02-18 22:23:56 +0545

    design changes

--- lib/src/form_builder/varicon_form_builder.dart
+++ lib/src/form_builder/varicon_form_builder.dart
@@ -192,6 +192,8 @@ class VariconFormBuilderState extends ConsumerState<VariconFormBuilder> {
         body: Padding(
           padding: widget.padding ?? const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Expanded(
                 child: SingleChildScrollView(
